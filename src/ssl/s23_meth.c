de9faaf83cf8300386ece252c5b0731eea17e1b9
jsing <jsing>
2014-04-14 15:15:33 +0000

    First pass at applying KNF to the OpenSSL code, which almost makes it readable. This pass is whitespace only and can readily be verified using tr and md5.

--- src/ssl/s23_meth.c
+++ src/ssl/s23_meth.c
@@ -1,4 +1,4 @@
-/* ssl/s23_pkt.c */
+/* ssl/s23_meth.c */
 /* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
  * All rights reserved.
  *
@@ -57,58 +57,35 @@
  */
 
 #include <stdio.h>
-#include <errno.h>
+#include <openssl/objects.h>
 #include "ssl_locl.h"
-#include <openssl/evp.h>
-#include <openssl/buffer.h>
 
-int
-ssl23_write_bytes(SSL *s)
+static const SSL_METHOD *ssl23_get_method(int ver);
+static const SSL_METHOD
+*ssl23_get_method(int ver)
 {
-	int i, num, tot;
-	char *buf;
-
-	buf = s->init_buf->data;
-	tot = s->init_off;
-	num = s->init_num;
-	for (;;) {
-		s->rwstate = SSL_WRITING;
-		i = BIO_write(s->wbio, &(buf[tot]), num);
-		if (i <= 0) {
-			s->init_off = tot;
-			s->init_num = num;
-			return (i);
-		}
-		s->rwstate = SSL_NOTHING;
-		if (i == num)
-			return (tot + i);
-
-		num -= i;
-		tot += i;
-	}
+#ifndef OPENSSL_NO_SSL2
+	if (ver == SSL2_VERSION)
+		return (SSLv2_method());
+	else
+#endif
+#ifndef OPENSSL_NO_SSL3
+	if (ver == SSL3_VERSION)
+		return (SSLv3_method());
+	else
+#endif
+#ifndef OPENSSL_NO_TLS1
+	if (ver == TLS1_VERSION)
+		return (TLSv1_method());
+	else if (ver == TLS1_1_VERSION)
+		return (TLSv1_1_method());
+	else if (ver == TLS1_2_VERSION)
+		return (TLSv1_2_method());
+	else
+#endif
+	return (NULL);
 }
 
-/* return regularly only when we have read (at least) 'n' bytes */
-int
-ssl23_read_bytes(SSL *s, int n)
-{
-	unsigned char *p;
-	int j;
+IMPLEMENT_ssl23_meth_func(SSLv23_method,
+    ssl23_accept, ssl23_connect, ssl23_get_method)
 
-	if (s->packet_length < (unsigned int)n) {
-		p = s->packet;
-
-		for (;;) {
-			s->rwstate = SSL_READING;
-			j = BIO_read(s->rbio,(char *)&(p[s->packet_length]),
-			n - s->packet_length);
-			if (j <= 0)
-				return (j);
-			s->rwstate = SSL_NOTHING;
-			s->packet_length += j;
-			if (s->packet_length >= (unsigned int)n)
-				return (s->packet_length);
-		}
-	}
-	return (n);
-}
