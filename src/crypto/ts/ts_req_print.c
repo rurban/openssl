634fd5c39171cc6fa954a1c14f82f724447023b2
jsing <jsing>
2014-04-21 16:32:06 +0000

    KNF.

--- src/crypto/ts/ts_req_print.c
+++ src/crypto/ts/ts_req_print.c
@@ -1,6 +1,6 @@
-/* crypto/ts/ts_verify_ctx.c */
+/* crypto/ts/ts_req_print.c */
 /* Written by Zoltan Glozik (zglozik@stones.com) for the OpenSSL
- * project 2003.
+ * project 2002.
  */
 /* ====================================================================
  * Copyright (c) 2006 The OpenSSL Project.  All rights reserved.
@@ -56,112 +56,49 @@
  *
  */
 
+#include <stdio.h>
 #include "cryptlib.h"
 #include <openssl/objects.h>
+#include <openssl/bn.h>
+#include <openssl/x509v3.h>
 #include <openssl/ts.h>
 
-TS_VERIFY_CTX *
-TS_VERIFY_CTX_new(void)
+/* Function definitions. */
+
+int
+TS_REQ_print_bio(BIO *bio, TS_REQ *a)
 {
-	TS_VERIFY_CTX *ctx = (TS_VERIFY_CTX *) malloc(sizeof(TS_VERIFY_CTX));
-
-	if (ctx)
-		memset(ctx, 0, sizeof(TS_VERIFY_CTX));
-	else
-		TSerr(TS_F_TS_VERIFY_CTX_NEW, ERR_R_MALLOC_FAILURE);
-	return ctx;
-}
-
-void
-TS_VERIFY_CTX_init(TS_VERIFY_CTX *ctx)
-{
-	OPENSSL_assert(ctx != NULL);
-	memset(ctx, 0, sizeof(TS_VERIFY_CTX));
-}
-
-void
-TS_VERIFY_CTX_free(TS_VERIFY_CTX *ctx)
-{
-	if (!ctx)
-		return;
-
-	TS_VERIFY_CTX_cleanup(ctx);
-	free(ctx);
-}
-
-void
-TS_VERIFY_CTX_cleanup(TS_VERIFY_CTX *ctx)
-{
-	if (!ctx)
-		return;
-
-	X509_STORE_free(ctx->store);
-	sk_X509_pop_free(ctx->certs, X509_free);
-
-	ASN1_OBJECT_free(ctx->policy);
-
-	X509_ALGOR_free(ctx->md_alg);
-	free(ctx->imprint);
-
-	BIO_free_all(ctx->data);
-
-	ASN1_INTEGER_free(ctx->nonce);
-
-	GENERAL_NAME_free(ctx->tsa_name);
-
-	TS_VERIFY_CTX_init(ctx);
-}
-
-TS_VERIFY_CTX *
-TS_REQ_to_TS_VERIFY_CTX(TS_REQ *req, TS_VERIFY_CTX *ctx)
-{
-	TS_VERIFY_CTX *ret = ctx;
-	ASN1_OBJECT *policy;
-	TS_MSG_IMPRINT *imprint;
-	X509_ALGOR *md_alg;
-	ASN1_OCTET_STRING *msg;
+	int v;
+	ASN1_OBJECT *policy_id;
 	const ASN1_INTEGER *nonce;
 
-	OPENSSL_assert(req != NULL);
-	if (ret)
-		TS_VERIFY_CTX_cleanup(ret);
-	else if (!(ret = TS_VERIFY_CTX_new()))
-		return NULL;
+	if (a == NULL)
+		return 0;
 
-	/* Setting flags. */
-	ret->flags = TS_VFY_ALL_IMPRINT & ~(TS_VFY_TSA_NAME | TS_VFY_SIGNATURE);
+	v = TS_REQ_get_version(a);
+	BIO_printf(bio, "Version: %d\n", v);
 
-	/* Setting policy. */
-	if ((policy = TS_REQ_get_policy_id(req)) != NULL) {
-		if (!(ret->policy = OBJ_dup(policy)))
-			goto err;
-	} else
-		ret->flags &= ~TS_VFY_POLICY;
+	TS_MSG_IMPRINT_print_bio(bio, TS_REQ_get_msg_imprint(a));
 
-	/* Setting md_alg, imprint and imprint_len. */
-	imprint = TS_REQ_get_msg_imprint(req);
-	md_alg = TS_MSG_IMPRINT_get_algo(imprint);
-	if (!(ret->md_alg = X509_ALGOR_dup(md_alg)))
-		goto err;
-	msg = TS_MSG_IMPRINT_get_msg(imprint);
-	ret->imprint_len = ASN1_STRING_length(msg);
-	if (!(ret->imprint = malloc(ret->imprint_len)))
-		goto err;
-	memcpy(ret->imprint, ASN1_STRING_data(msg), ret->imprint_len);
-
-	/* Setting nonce. */
-	if ((nonce = TS_REQ_get_nonce(req)) != NULL) {
-		if (!(ret->nonce = ASN1_INTEGER_dup(nonce)))
-			goto err;
-	} else
-		ret->flags &= ~TS_VFY_NONCE;
-
-	return ret;
-
-err:
-	if (ctx)
-		TS_VERIFY_CTX_cleanup(ctx);
+	BIO_printf(bio, "Policy OID: ");
+	policy_id = TS_REQ_get_policy_id(a);
+	if (policy_id == NULL)
+		BIO_printf(bio, "unspecified\n");
 	else
-		TS_VERIFY_CTX_free(ret);
-	return NULL;
+		TS_OBJ_print_bio(bio, policy_id);
+
+	BIO_printf(bio, "Nonce: ");
+	nonce = TS_REQ_get_nonce(a);
+	if (nonce == NULL)
+		BIO_printf(bio, "unspecified");
+	else
+		TS_ASN1_INTEGER_print_bio(bio, nonce);
+	BIO_write(bio, "\n", 1);
+
+	BIO_printf(bio, "Certificate required: %s\n",
+	    TS_REQ_get_cert_req(a) ? "yes" : "no");
+
+	TS_ext_print_bio(bio, TS_REQ_get_exts(a));
+
+	return 1;
 }
